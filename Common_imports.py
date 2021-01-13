ae4c666c283396093a047b842c8f05b66292fee8
shannon-np <9888155+shannon-np@users.noreply.github.com>
2021-01-13 15:08:26 -0600

    Create Common_imports.py

---
+++ Common_imports.py
@@ -0,0 +1,12 @@
+# so common you're not likely to forget
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import sklearn # large - probably aim for smaller pieces of it
+
+
+# from sklearn:
+from sklearn.utils import shuffle
+from sklearn.cross_validation import train_test_split
+from sklearn.metrics import accuracy_score # and other metrics
+from sklearn.svm import SVC # and other types of models
